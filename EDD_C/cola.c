2bf0d4eaec496942f4eea8495648564ecf7374ab
loconluis <luis.locon@gmail.com>
2016-11-25 00:57:01 -0600

    Se agregaron los metodos de la COLA en C

--- EDD_C/cola.c
+++ EDD_C/cola.c
@@ -5,39 +5,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*Estructura del nodo*/
 typedef struct Elemento{
   int dato;
   struct Elemento *sig;
 }Elemento;
 
+/*Declaraciones de Apuntadores*/
 Elemento *primero;
 Elemento *ultimo;
-
-/*Metodo de agregar elemento a la cola*/
-void push(Elemento* _elemento){
-    _elemento->sig = NULL;
-    //Comprobar si la cola esta vacia
-    if(primer == NULL){
-        primero = _elemento;
-        ultimo = _elemento;
-    }else{
-        ultimo->sig = _elemento;
-        ultimo = _elemento;
-    }
-}
-
-/*Metodo de eliminar elemento a la cola*/
-Elemento* pop(){
-    if(primero == NULL){
-        return NULL;
-    }
-    
-    Elemento* aux = primero;
-    primero = primero->sig;
-    return aux;
-}
 
 int main(){
 
     return 0;
+}
+
+
+/*Aparta la memoria de un nuevo elemento*/
+Elemento *Cola(){
+  Elemento *e = (Elemento *)malloc(sizeof(Elemento));
+  if (e==NULL){
+    printf("Falta Memoria");
+    exit(0);
+  }
+  return e;
+}
+
+/*Metodo de agregar elemento a la cola*/
+void push(int dato){
+    Elemento *e = Cola();
+    e->sig = NULL;
+    //Comprobar si la cola esta vacia
+    if(primero == NULL){
+        primero = e;
+        ultimo = e;
+    }else{
+        ultimo->sig = e;
+        ultimo = e;
+    }
+}
+
+/*Metodo de eliminar elemento a la cola*/
+int pop(void){
+    Elemento *aux;
+    int x;
+    if(primero == NULL){
+        puts("La cola esta vacia");
+        return EOF;
+    }else{
+        x = primero->dato;
+        aux = primero;
+        primero = primero->sig;
+        free(aux);
+        return x;
+    }
+    printf("\n");
+}
+
+/*Imprimir la Cola*/
+void printQueu(Elemento *primero){
+    printf("Los datos de la Cola son:\n");
+    while(primero!=NULL){
+        printf("%d \n", primero->dato);
+        primero=primero->sig;
+    }
+    printf("\n");
+}
+
+/*Ver la cabeza de la Cola*/
+void verCabeza(){
+    int i;
+    Elemento *cabeza;
+    cabeza = primero;
+    while(f!=NULL){
+        i = cabeza->dato;
+    }
+    printf("La cabeza de la cola es: \n %d", i);
+    printf("\n");
 }
